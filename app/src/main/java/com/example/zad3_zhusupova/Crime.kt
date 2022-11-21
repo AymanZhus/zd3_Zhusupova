4cdeba9c0a47a1bd6b2d851158e9e7171822ccca
AymanZhus <Zhusupova106@gmail.com>
2022-11-21 10:41:50 +0500

    Add exercise Six

--- app/src/main/java/com/example/zad3_zhusupova/Crime.kt
+++ app/src/main/java/com/example/zad3_zhusupova/Crime.kt
@@ -9,4 +9,5 @@ import androidx.room.PrimaryKey
 data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
                  var title: String = "",
                  var date: Date = Date(),
-                 var isSolved: Boolean = false)
+                 var isSolved: Boolean = false,
+                 var suspect: String = "")
--- app/src/main/java/com/example/zad3_zhusupova/CrimeRepository.kt
+++ app/src/main/java/com/example/zad3_zhusupova/CrimeRepository.kt
@@ -4,18 +4,19 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.example.zad3_zhusupova.database.CrimeDatabase
+import com.example.zad3_zhusupova.database.migration_1_2
 import java.util.*
 import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "crime-database"
 
-class CrimeRepository private
-constructor(context: Context) {
+class CrimeRepository private constructor(context: Context) {
     private val database : CrimeDatabase = Room.databaseBuilder(
         context.applicationContext,
         CrimeDatabase::class.java,
         DATABASE_NAME
-    ).build()
+    ).addMigrations(migration_1_2)
+        .build()
     private val crimeDao = database.crimeDao()
     private val executor = Executors.newSingleThreadExecutor()
     fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
